59bea02690e4777963627a8d2623fe0ba1a564ce
Varthan345 <srivarthanan535@gmail.com>
2023-07-13 11:45:02 +0530

    while

--- d12-20230708/assignment/p17.py
+++ d12-20230708/assignment/p17.py
@@ -1,6 +1,8 @@
 print("WELCOME TO THE BANK OF BRUDA")
-your_pin=int(input("ENTER YOUR PIN:"))
-if your_pin==1234:
-    print("PIN ACCEPTED.YOU HAVE ACCESS TO YOUR ACCOUNT")
-else:
-    print("INCORRECT PIN.try again")
+while True:
+    your_pin=int(input("\nENTER YOUR PIN:"))
+    if your_pin==1234:
+        print("PIN ACCEPTED.YOU HAVE ACCESS TO YOUR ACCOUNT")
+        break
+    else:
+        print("INCORRECT PIN.try again\n")
